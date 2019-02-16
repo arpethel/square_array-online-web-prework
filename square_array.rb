9751e2f8be90d9a3f6708a34a4e690d52f6d9510
Ryan Pethel <arpethel@gmail.com>
2019-02-16 22:01:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  i = 0
+  my_arr = []
+  array.each do |ele|
+    new_ele = Math.sqrt(ele)
+    my_arr << new_ele
+    i += 1
+  end
+  return my_arr
 end
