ce18a405cbad1494103b69d37a77f02c3d583a3b
Ryan Pethel <arpethel@gmail.com>
2019-02-16 22:06:20 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,17 @@
+# def square_array(array)
+#   i = 0
+#   my_arr = []
+#   array.each do |ele|
+#     new_ele = (ele * ele)
+#     my_arr << new_ele
+#     i += 1
+#   end
+#   return my_arr
+# end
+
+
 def square_array(array)
-  i = 0
   my_arr = []
-  array.each do |ele|
-    new_ele = (ele * ele)
-    my_arr << new_ele
-    i += 1
-  end
+  my_arr << array.collect { |ele| ele * ele }
   return my_arr
 end
